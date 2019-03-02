3ae9d2dc519e8eefd65b7f754b5c423d3b91b921
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2019-03-02 00:28:15 -0600

    counter example with parcel and bulma

---
+++ README.md
@@ -0,0 +1,10 @@
+# Elm Hello World
+> A counter example using Elm!
+
+### Local Development
+
+1. __`npm install`__
+
+1. __`npm start`__
+
+1. Your website should be running at [http://localhost:1234](http://localhost:1234)
---
+++ src/Main.elm
@@ -0,0 +1,57 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, button, div, h1, h2, text)
+import Html.Attributes exposing (class, id)
+import Html.Events exposing (onClick)
+
+
+main : Program () Int Msg
+main =
+    Browser.sandbox
+        { init = initialModel
+        , update = update
+        , view = view
+        }
+
+
+initialModel : Int
+initialModel =
+    0
+
+
+view : Int -> Html Msg
+view model =
+    div [ class "hero is-dark" ]
+        [ div
+            [ class "hero-body" ]
+            [ div
+                [ class "container" ]
+                [ h2 [ class "title is-1" ] [ text (String.fromInt model) ]
+                , div [ class "buttons" ]
+                    [ button [ class "button", onClick Plus ] [ text "+" ]
+                    , button [ class "button", onClick Minus ] [ text "-" ]
+                    , button [ class "button", onClick Reset ] [ text "reset" ]
+                    ]
+                ]
+            ]
+        ]
+
+
+type Msg
+    = Plus
+    | Minus
+    | Reset
+
+
+update : Msg -> Int -> Int
+update msg model =
+    case msg of
+        Plus ->
+            model + 1
+
+        Minus ->
+            model - 1
+
+        Reset ->
+            0
